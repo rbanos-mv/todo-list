abbd01a762c48b687046c5cf53c154d344345007
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-26 11:16:16 -0500

    Create the TaskList class

---
+++ src/TaskList.js
@@ -0,0 +1,14 @@
+class TaskList {
+  static #taskList = [
+    { description: 'aaa', completed: true, index: 1 },
+    { description: 'bbb', completed: true, index: 2 },
+    { description: 'ccc', completed: false, index: 3 },
+    { description: 'ddd', completed: false, index: 4 },
+  ];
+
+  static getTaskList() {
+    return this.#taskList;
+  }
+}
+
+export default TaskList;
