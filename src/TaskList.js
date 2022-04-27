6a4bbbd5052ffabc95b61b93bc0c6b211650581d
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-27 07:05:34 -0500

    Remove all hardcoded items from the tasks array.

--- src/TaskList.js
+++ src/TaskList.js
@@ -1,10 +1,5 @@
 class TaskList {
-  static #taskList = [
-    { description: 'aaa', completed: true, index: 1 },
-    { description: 'bbb', completed: true, index: 2 },
-    { description: 'ccc', completed: false, index: 3 },
-    { description: 'ddd', completed: false, index: 4 },
-  ];
+  static #taskList = [];
 
   static getTaskList = () => this.#taskList;
 }
