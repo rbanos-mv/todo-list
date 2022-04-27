2b542f8b9dfe83b450b124e0746aeb002534d7c8
Roberto A. Baños Alvarez <roberto.banos.mv@gmail.com>
2022-04-27 08:06:07 -0500

    Implement a function for adding a new task

--- src/TaskList.js
+++ src/TaskList.js
@@ -2,6 +2,13 @@ class TaskList {
   static #taskList = [];
 
   static getTaskList = () => this.#taskList;
+
+  static add = (task) => {
+    const index = (TaskList.#taskList.length + 1).toString();
+    const newTask = { ...task, completed: false, index };
+    TaskList.#taskList.push(newTask);
+    return newTask;
+  };
 }
 
 export default TaskList;
--- src/UI.js
+++ src/UI.js
@@ -1,22 +1,51 @@
 import TaskList from './TaskList.js';
-import more from './more.svg';
+import deleteIco from './delete.svg';
+import moreIco from './more.svg';
 
 class UI {
   static #heading = document.querySelector('.heading');
 
   static #newItem = document.querySelector('.input-row');
 
-  static #taskInput = document.querySelector('#task-input');
+  static #taskNew = document.querySelector('#task-new');
 
   static #taskList = document.querySelector('#todo-list');
 
   static #template = document.querySelector('li');
 
+  static getCheckBox = (taskElement) => taskElement.querySelector('input[type="checkbox"]');
+
+  static getDeleBtn = (taskElement) => taskElement.querySelector('#dele');
+
+  static getMoreBtn = (taskElement) => taskElement.querySelector('#more');
+
+  static getTaskEdit = (taskElement) => taskElement.querySelector('#task-edit');
+
+  static setActionButtons(taskElement, index) {
+    const deleBtn = UI.getDeleBtn(taskElement);
+    deleBtn.src = deleteIco;
+    deleBtn.textContent = index;
+
+    const moreBtn = UI.getMoreBtn(taskElement);
+    moreBtn.src = moreIco;
+    moreBtn.textContent = index;
+  }
+
+  static showActionButton = (taskElement, showMore) => {
+    const deleBtn = UI.getDeleBtn(taskElement);
+    deleBtn.classList.toggle('hidden', showMore);
+    const moreBtn = UI.getMoreBtn(taskElement);
+    moreBtn.classList.toggle('hidden', !showMore);
+  };
+
   static displayTask = (task) => {
     const taskElement = UI.#template.cloneNode(true);
-    taskElement.querySelector('input').checked = task.completed;
-    taskElement.querySelector('label').lastChild.textContent = task.description;
-    taskElement.querySelector('img').src = more;
+    UI.getCheckBox(taskElement).checked = task.completed;
+    const taskEdit = UI.getTaskEdit(taskElement);
+    taskEdit.value = task.description;
+    UI.setActionButtons(taskElement, task.index);
+    UI.showActionButton(taskElement, true);
+
     UI.#taskList.appendChild(taskElement);
   };
 
@@ -27,6 +56,18 @@ class UI {
 
   static setup = () => {
     UI.diplayTaskList();
+    UI.#taskNew.addEventListener('keypress', UI.add);
+  };
+
+  static add = (event) => {
+    const editElement = event.target;
+    if (event.key === 'Enter' && editElement.value) {
+      const description = UI.#taskNew.value;
+      const newTask = TaskList.add({ description });
+      UI.displayTask(newTask);
+      UI.#taskNew.value = '';
+      event.preventDefault();
+    }
   };
 }
 
